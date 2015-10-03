748ae2e24b3cfb81ab2fd9be609959e36b701734
jonfor <jonforscher@gmail.com>
2015-10-02 22:44:29 -0400

    Added sendServerInfo POST method.

--- app/src/main/java/net/jonfor/poke4000/networking/RequestUtil.java
+++ app/src/main/java/net/jonfor/poke4000/networking/RequestUtil.java
@@ -7,7 +7,11 @@ import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by jonfor on 9/29/15.
@@ -41,6 +45,48 @@ public class RequestUtil {
                             Log.e("sendPushToken", error.toString());
                             Toast.makeText(context, "An error has occurred while pushing register " +
                                             "token to database",
+                                    Toast.LENGTH_LONG).show();
+                            error.printStackTrace();
+                        }
+                    }
+                });
+
+        mInstance = Poke4000Singleton.getInstance(context);
+        mInstance.addToRequestQueue(request);
+    }
+
+    public static void sendServerInfo(String ip, int port, int frequency, final Context context) {
+        String url = host + "/data";
+
+
+        JSONObject data = new JSONObject();
+        try {
+            data.put("ip", ip);
+            data.put("port", port);
+            data.put("frequency", frequency);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, data,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error.networkResponse != null) {
+                            Log.e("sendServerInfo", error.toString());
+                            Toast.makeText(context, "An error has occurred while pushing server info " +
+                                            "to database",
+                                    Toast.LENGTH_LONG).show();
+                            error.printStackTrace();
+                        } else {
+                            Log.e("sendServerInfo", error.toString());
+                            Toast.makeText(context, "An error has occurred while pushing server info " +
+                                            "to database",
                                     Toast.LENGTH_LONG).show();
                             error.printStackTrace();
                         }
